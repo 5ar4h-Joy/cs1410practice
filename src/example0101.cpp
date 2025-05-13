a836c1b9e98f22fac9b3e7ccb3131cb751c5d9d8
5ar4h <68246013+5ar4h-Joy@users.noreply.github.com>
2025-05-13 03:45:29 +0000

    Example one finished

---
+++ src/example0101.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x, y;
+    cout << "enter x:";
+    cin >> x;
+    cout << "enter y:";
+    cin >> y;
+    cout << "x + y=" << (x + y) << endl;
+    cout << "x - y=" << (x - y) << endl;
+    cout << "x * y=" << (x * y) << endl;
+    cout << "x / y=" << (x / y) << endl;
+    return 0;
+}
